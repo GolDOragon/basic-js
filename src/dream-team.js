812116982e16253dc8c79276a7126ed1438785d0
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-02-26 20:31:39 +0300

    feat: solve "Dream team" task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false;
+
+  let getFirstLetter = function(str) {
+    let first_letter_number = str.search(/[A-Za-z]/);
+    return word[first_letter_number];
+  };
+
+  let team_name = [];
+
+  for (word of members) {
+    if (typeof word != "string") continue;
+
+    let letter = getFirstLetter(word);
+    team_name.push(letter.toUpperCase());
+  }
+
+  return team_name.sort().join("");
 };
