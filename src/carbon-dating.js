f93c71b82ae55d49f4682716cfe7f5945e7f7f9f
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-02-26 19:34:57 +0300

    feat: solve "Carbon dating" task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,18 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity != "string" ||
+    /[A-Za-z]/.test(sampleActivity) ||
+    parseFloat(sampleActivity) > 15 ||
+    parseFloat(sampleActivity) <= 0
+  )
+    return false;
+
+  let time = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / K;
+  time = Math.ceil(time);
+
+  return time;
 };
