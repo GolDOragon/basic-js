f34bd59bd3154bc7a79114658af790cbef8d6460
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-02-27 14:29:00 +0300

    feat: solve "Transform array" task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!(arr instanceof Array)) throw new Error();
+
+  let new_array = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+
+      case "--discard-next":
+        i++;
+        break;
+
+      case "--discard-prev":
+        new_array.pop();
+        break;
+
+      case "--double-next":
+        if (i+1 == arr.length) break;
+
+        new_array.push(arr[i + 1]);
+        break;
+
+      case "--double-prev":
+        if (i-1 == -1) break;
+
+        new_array.push(arr[i - 1]);
+        break;
+
+      default:
+        new_array.push(arr[i]);
+    }
+  }
+  return new_array;
 };
