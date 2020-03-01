8b0be3e6fc089171b5d77454168440a4a32682e3
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-03-01 13:13:17 +0300

    feat: solve "Chain maker" task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,42 @@
 const chainMaker = {
+  all_links: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.all_links.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value == null) {
+      this.all_links.push(String(value));
+    } else {
+      this.all_links.push(value);
+    }
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (
+      position < 1 ||
+      (position > this.all_links.length) || (+position != position)
+    ) {
+      this.all_links = [];
+      throw new Error();
+    }
+
+    this.all_links.splice(position - 1, 1);
+    return this;
   },
+  
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.all_links = this.all_links.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    let result = "( " + this.all_links.join(" )~~( ") + " )";
+    this.all_links = [];
+    return result;
+  },
 };
 
 module.exports = chainMaker;
