bd4b25dc07e7b2c1df1e01a734551319ff2eae7b
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-03-02 16:44:10 +0300

    feat: solve "Extended repeater" task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,40 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addition = "";
+  if (Object.keys(options).includes("addition")) { // if addition is passed
+    addition = String(options.addition);
+  }
+
+  let repeatTimes = options.repeatTimes || 1;
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  let separator = options.separator || "+";
+  let additionSeparator = options.additionSeparator || "|";
+
+  function repeat(inside_options) {
+    let edit_str = inside_options.edit_str;
+    let times = inside_options.times || 1;
+    let sep = inside_options.sep || "+";
+    let add = inside_options.add || "";
+
+    let result = [];
+    for (let i = 0; i < times; i++) {
+      result.push(edit_str + add);
+    }
+    return result.join(sep);
+  }
+
+  addition = repeat({
+    edit_str: addition,
+    times: additionRepeatTimes,
+    sep: additionSeparator
+  });
+
+  let answer = repeat({
+    edit_str: str,
+    times: repeatTimes,
+    sep: separator,
+    add: addition
+  });
+
+  return answer;
 };
-  
